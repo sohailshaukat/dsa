3852d420e4fa418d7efa24ce06b03cab6da31d70
Mohamed Sohail <sohail47k@gmail.com>
2024-06-09 14:57:19 -0400

    updating function

--- Recursion/4.4-Binary-Search-Using-Recursion/code.py
+++ Recursion/4.4-Binary-Search-Using-Recursion/code.py
@@ -12,7 +12,8 @@ def search(arr, el):
         else:
             return mid + 1 + search(arr[mid+1:], el)
     except :
-        return 'Not found'
+        return None
 
-arr = [1,2,3,4,5,6,7,8,9,10]
-print(search(arr, 0))
+arr = list(range(1,11))
+for i in range(0,11):
+    print(search(arr, i))
